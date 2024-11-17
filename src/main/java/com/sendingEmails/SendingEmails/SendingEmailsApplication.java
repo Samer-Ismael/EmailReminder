0b6a07188ce7206f88aa741f5deefe78c73d339e
Samer-Ismael <samerismaeelbat2@hotmail.com>
2024-11-17 17:44:05 +0100

    Adding function to open browser automatically

--- src/main/java/com/sendingEmails/SendingEmails/SendingEmailsApplication.java
+++ src/main/java/com/sendingEmails/SendingEmails/SendingEmailsApplication.java
@@ -10,6 +10,23 @@ public class SendingEmailsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SendingEmailsApplication.class, args);
+		openBrowser("http://localhost:8080");
+
 	}
 
+	private static void openBrowser(String url) {
+		String os = System.getProperty("os.name").toLowerCase();
+
+		try {
+			if (os.contains("win")) {
+				Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start", url});
+			} else if (os.contains("mac")) {
+				Runtime.getRuntime().exec(new String[]{"open", url});
+			} else {
+				System.err.println("Unsupported operating system: " + os);
+			}
+		} catch (Exception e) {
+			System.err.println("Failed to open browser: " + e.getMessage());
+		}
+	}
 }
